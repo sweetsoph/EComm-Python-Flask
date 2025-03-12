c3ff4c7484f736dc2f464b1142b136977f6ec5ec
Sophia Verardo <sophia.verardods@gmail.com>
2025-03-11 22:31:16 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# Importação
+from flask import Flask
+
+app = Flask(__name__)
+
+# Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/teste')
+def teste():
+    return 'Teste'
+
+if __name__ == "__main__":
+    app.run(debug=True)
