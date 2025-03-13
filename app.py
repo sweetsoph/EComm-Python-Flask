f97b1fe96fb197d0303d301bb7d5fd4b933a52d3
Sophia Verardo <sophia.verardods@gmail.com>
2025-03-13 12:43:24 -0300

    Adicionando autenticação de usuários ao sistema

--- app.py
+++ app.py
@@ -2,12 +2,20 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+# O login manager permite que o processo de autenticação de um usuário seja bem mais rápida e eficiente
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 # Inicialização do Flask
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'MY-SECRET-KEY-123' # Chave secreta para o LoginManager
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db' # Configuração do banco de dados
+
+# Instanciando o LoginManager
+login_manager = LoginManager()
+
 db = SQLAlchemy(app) # Inicialização do banco de dados
+login_manager.init_app(app) # Inicializando o LoginManager
+login_manager.login_view = 'login' # Rota de login
 CORS(app) # Habilita o CORS
 
 # 
@@ -21,6 +29,12 @@ class Product(db.Model):
     name = db.Column(db.String(50), nullable=False) # Limita o tamanho do campo
     description = db.Column(db.Text, nullable=True) # Campo não obrigatório, não limita o tamanho
 
+# Tabela de Usuários
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
 # 
 # ROTAS
 # 
@@ -29,6 +43,28 @@ class Product(db.Model):
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+# Autenticação
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login/', methods=['POST'])
+def login():
+    data = request.json
+
+    user = User.query.filter_by(username=data['username']).first()
+    if user and user.password == data['password']:
+        login_user(user)
+        return jsonify({'message': 'Usuário autenticado com sucesso!'})
+    else:
+        return jsonify({'error': 'Usuário ou senha inválidos'}), 401
+    
+@app.route('/logout/', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Usuário deslogado com sucesso!'})
 
 @app.route('/api/products/', methods=['GET'])
 def get_products():
@@ -43,6 +79,7 @@ def get_products():
     return jsonify(products_json)
 
 @app.route('/api/products/add/', methods=['POST'])
+@login_required
 def add_product():
     try:
         data = request.json
@@ -57,6 +94,7 @@ def add_product():
         return jsonify({'error': str(e)}), 500
     
 @app.route('/api/products/delete/<int:product_id>/', methods=['DELETE'])
+@login_required
 def delete_product(product_id):
     # Recuperar o produto na base de dados
     product = Product.query.get(product_id)
@@ -81,9 +119,9 @@ def get_product_details(product_id):
         })
     else:
         return jsonify({'error': 'Produto não encontrado'}), 404
-    
-    
+        
 @app.route('/api/products/update/<int:product_id>/', methods=['PUT'])
+@login_required
 def update_product(product_id):
     try:
         product = Product.query.get(product_id)
